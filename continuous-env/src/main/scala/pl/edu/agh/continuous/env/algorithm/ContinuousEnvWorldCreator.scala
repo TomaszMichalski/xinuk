4b1e42206865461b4189612107b6bdc43441a106
TomaszMichalski <tomasz.michalski19@gmail.com>
2021-03-10 13:49:47 +0100

    Example of signal propagation

--- continuous-env/src/main/scala/pl/edu/agh/continuous/env/algorithm/ContinuousEnvWorldCreator.scala
+++ continuous-env/src/main/scala/pl/edu/agh/continuous/env/algorithm/ContinuousEnvWorldCreator.scala
@@ -3,7 +3,7 @@ package pl.edu.agh.continuous.env.algorithm
 import pl.edu.agh.continuous.env.config.ContinuousEnvConfig
 import pl.edu.agh.continuous.env.model.ContinuousEnvCell
 import pl.edu.agh.xinuk.algorithm.WorldCreator
-import pl.edu.agh.xinuk.model.{CellContents, CellState, Signal, WorldBuilder}
+import pl.edu.agh.xinuk.model.{CellContents, CellState, Obstacle, Signal, WorldBuilder}
 import pl.edu.agh.xinuk.model.grid.{GridCellId, GridWorldBuilder}
 
 import scala.util.Random
@@ -19,10 +19,12 @@ object ContinuousEnvWorldCreator extends WorldCreator[ContinuousEnvConfig] {
       x <- 0 until config.worldWidth
       y <- 0 until config.worldHeight
     } {
-      val contents: CellContents = if (random.nextDouble() < config.signalSpawnChance) {
-        ContinuousEnvCell(config.initialSignal)
+      val contents: CellContents = if (x == 20 && y == 20) {
+        ContinuousEnvCell(config.initialSignal, 1.0)
+      }else if ((y == 25 && x > 15 && x < 25) || (x == 22 && y < 15 && y > 5) || (x == 22 && y > 17 && y < 22)) {
+        ContinuousEnvCell(Signal.zero, 0.0)
       } else {
-        ContinuousEnvCell(Signal.zero)
+        ContinuousEnvCell(Signal.zero, 1.0)
       }
 
       worldBuilder(GridCellId(x, y)) = CellState(contents)
--- continuous-env/src/main/scala/pl/edu/agh/continuous/env/model/ContinuousEnvCell.scala
+++ continuous-env/src/main/scala/pl/edu/agh/continuous/env/model/ContinuousEnvCell.scala
@@ -3,7 +3,10 @@ package pl.edu.agh.continuous.env.model
 import pl.edu.agh.xinuk.config.XinukConfig
 import pl.edu.agh.xinuk.model.{CellContents, Signal}
 
-final case class ContinuousEnvCell(initialSignal: Signal) extends CellContents {
+final case class ContinuousEnvCell(initialSignal: Signal, signalFactor: Double) extends CellContents {
   override def generateSignal(iteration: Long)(implicit config: XinukConfig): Signal =
     initialSignal
+
+  override def signalFactor(iteration: Long)(implicit config: XinukConfig): Double =
+    signalFactor
 }

4289838b335cde079373ff644108a27cae3d69be
TomaszMichalski <tomasz.michalski19@gmail.com>
2021-05-26 21:09:19 +0200

    20x20 discrete simulation

--- continuous-env/src/main/scala/pl/edu/agh/continuous/env/algorithm/ContinuousEnvWorldCreator.scala
+++ continuous-env/src/main/scala/pl/edu/agh/continuous/env/algorithm/ContinuousEnvWorldCreator.scala
@@ -20,9 +20,9 @@ object ContinuousEnvWorldCreator extends WorldCreator[ContinuousEnvConfig] {
       y <- 0 until config.worldHeight
     } {
 
-      val contents: CellContents = if (x == 5 && y == 5) {
+      val contents: CellContents = if (x == 9 && y == 9) {
         ContinuousEnvCell(config.initialSignal)
-      } else if (x == 1 && y == 9) {
+      } else if (x == 1 && y == 18) {
         ContinuousEnvBeing()
       } else if (isObstacle(x, y)) {
         Obstacle
@@ -38,25 +38,45 @@ object ContinuousEnvWorldCreator extends WorldCreator[ContinuousEnvConfig] {
 
   private def isObstacle(x: Int, y: Int): Boolean = {
     val result = if (x == 0) {
-      y == 0 || y == 1 || y == 9
+      y <= 5 || y >= 18
     } else if (x == 1) {
-      y == 0 || (y >= 3 && y <= 6)
+      y <= 2 || y == 19
     } else if (x == 2) {
-      y == 2 || y == 3 || (y >= 6 && y <= 8)
+      y <= 1 || (y >= 9 && y <= 13)
     } else if (x == 3) {
-      (y >= 1 && y <= 2) || y >= 7
+      y == 0 || (y >= 7 && y <= 15)
     } else if (x == 4) {
-      y == 1 || (y >= 4 && y <= 6) || y == 9
+      y == 0 || (y >= 5 && y <= 16)
     } else if (x == 5) {
-      (y >= 1 && y <= 2) || y == 4 || (y >= 6 && y <= 7) || y == 9
+      y == 0 || (y >= 4 && y <= 7) || (y >= 15 && y <= 18)
     } else if (x == 6) {
-      y == 2 || y == 6 || y == 7 || y == 9
+      y == 4 || y == 5 || y >= 17
     } else if (x == 7) {
-      y == 0 || (y >= 2 && y <= 6) || y == 9
+      (y >= 3 && y <= 5) || (y >= 8 && y <= 13) || y >= 18
     } else if (x == 8) {
-      y == 0 || y >= 8
+      y == 3 || y == 4 || (y >= 7 && y <= 14) || y >= 18
     } else if (x == 9) {
-      y <= 2 || y >= 7
+      y == 3 || y == 4 || y == 7 || y == 8 || y == 14 || y == 15 || y >= 18
+    } else if (x == 10) {
+      y == 3 || y == 4 || y == 7 || y == 8 || y == 9 || y == 14 || y == 15 || y >= 18
+    } else if (x == 11) {
+      y == 3 || y == 4 || y == 7 || y == 8 || y == 9 || y == 14 || y == 15 || y >= 18
+    } else if (x == 12) {
+      y == 3 || y == 4 || y == 14 || y == 15 || y >= 18
+    } else if (x == 13) {
+      y == 0 || y == 4 || y == 5 || (y >= 13 && y <= 15) || y >= 18
+    } else if (x == 14) {
+      y == 0 || (y >= 5 && y <= 14) || y >= 18
+    } else if (x == 15) {
+      y == 0 || y == 1 || (y >= 6 && y <= 13) || y >= 17
+    } else if (x == 16) {
+      y <= 2 || y >= 17
+    } else if (x == 17) {
+      y <= 4 || y >= 15
+    } else if (x == 18) {
+      true
+    } else if (x == 19) {
+      true
     } else {
       false
     }
